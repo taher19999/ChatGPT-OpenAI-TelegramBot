f3b59a7fb96f6b58929ccff65bf6a46fb3825bca
taher19999 <94671425+taher19999@users.noreply.github.com>
2023-02-27 18:40:36 +0300

    Update helpHandler.js

--- handlers/helpHandler.js
+++ handlers/helpHandler.js
@@ -2,9 +2,9 @@ import catchAsync from '../utils/catchAsync.js';
 
 export default catchAsync(async ctx => {
   await ctx.reply(
-    '\u{1F916}Commands!\
-    \nType the next commands to talk with me!\
-     \n\n/ask - Ask me what you want.\
-  \n\n/exit - Close conversation.'
+    '\u{1F916}الإوامر!\
+    \nاكتب الأوامر التالية للتحدث معي!\
+     \n\n/ask -اسألني ماذا تريد.\
+  \n\n/exit - محادثة مباشرة.'
   );
 });
